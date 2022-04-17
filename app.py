a748d49645ecd1deecc7c071574a1751b2d8fa86
arartawil <arartawil@yahoo.com>
2022-04-18 01:29:26 +0300

    Pima Indian Diabetes Prediction App

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import Food ,Data
+from apps import Food ,Data,Pima_app
 
 app = MultiApp()
 
@@ -12,5 +12,6 @@ This multi-page apps Food Recommendation app and Data profiling App.
 # Add all your application here
 app.add_app("Food", Food.app)
 app.add_app("Data Profiling", Data.app)
+app.add_app("Diabetes Prediction", Pima_app.app)
 # The main app
 app.run()
